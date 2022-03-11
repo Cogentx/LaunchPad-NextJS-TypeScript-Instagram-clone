937c7f83bfec4c8d4d1751375df78dc7054ccad1
Cogentx <code@cogentx.com>
2022-03-11 19:46:46 -0330

    Header - middle including Search input box

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import {
   SearchIcon,
   PlusCircleIcon,
@@ -6,32 +6,43 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
   MenuIcon,
-} from '@heroicons/react/outline'
+} from '@heroicons/react/outline';
 
 export default function Header() {
   return (
     // <div>
     <div className="flex max-w-6xl items-center justify-between bg-white">
       {/* Left */}
-      <div className="relative hidden h-24 w-24 lg:inline-grid">
-        <Image
-          src="https://links.papareact.com/ocw"
-          alt="Instagram Logo"
-          layout="fill"
-          objectFit="contain"
-        />
-      </div>
-      <div className="relative h-10 w-10 flex-shrink-0 cursor-pointer lg:hidden">
-        <Image
-          src="https://links.papareact.com/jjm"
-          alt="Instagram Logo"
-          layout="fill"
-          objectFit="contain"
-        />
+      <div>
+        <div className="relative hidden h-24 w-24 lg:inline-grid">
+          <Image
+            src="https://links.papareact.com/ocw"
+            alt="Instagram Logo"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative h-10 w-10 flex-shrink-0 cursor-pointer lg:hidden">
+          <Image
+            src="https://links.papareact.com/jjm"
+            alt="Instagram Logo"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
       </div>
       {/* Center - Search Input */}
-      <div>
-        <h1>Center</h1>
+      <div className="max-w-xs">
+        <div className="relative mt-1 rounded-md p-3">
+          <div className="pointer-events-none absolute inset-y-0 flex items-center pl-3">
+            <SearchIcon className="h-5 w-5 text-gray-500" />
+          </div>
+          <input
+            className="block w-full rounded-md border-gray-300 bg-gray-50 pl-10 focus:border-black focus:ring-black sm:text-sm"
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
       </div>
       {/* Right */}
       <div>
