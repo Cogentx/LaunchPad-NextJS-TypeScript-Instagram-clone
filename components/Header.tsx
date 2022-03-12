16c207c36f8bfd329a6725d1add3878036f7d346
Cogentx <code@cogentx.com>
2022-03-12 14:53:33 -0330

    Header - right section with custom styled icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from 'next/image'
 import {
   SearchIcon,
   PlusCircleIcon,
@@ -6,49 +6,55 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
   MenuIcon,
-} from '@heroicons/react/outline';
+} from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
 
 export default function Header() {
   return (
-    // <div>
-    <div className="flex max-w-6xl items-center justify-between bg-white">
-      {/* Left */}
-      <div>
-        <div className="relative hidden h-24 w-24 lg:inline-grid">
-          <Image
-            src="https://links.papareact.com/ocw"
-            alt="Instagram Logo"
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-        <div className="relative h-10 w-10 flex-shrink-0 cursor-pointer lg:hidden">
-          <Image
-            src="https://links.papareact.com/jjm"
-            alt="Instagram Logo"
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-      </div>
-      {/* Center - Search Input */}
-      <div className="max-w-xs">
-        <div className="relative mt-1 rounded-md p-3">
-          <div className="pointer-events-none absolute inset-y-0 flex items-center pl-3">
-            <SearchIcon className="h-5 w-5 text-gray-500" />
+    <div>
+      <div className="flex max-w-6xl items-center justify-between bg-white px-5 lg:mx-auto">
+        {/* Left */}
+        <div>
+          <div className="relative hidden h-24 w-24 lg:inline-grid">
+            <Image
+              src="https://links.papareact.com/ocw"
+              alt="Instagram Logo"
+              layout="fill"
+              objectFit="contain"
+            />
           </div>
-          <input
-            className="block w-full rounded-md border-gray-300 bg-gray-50 pl-10 focus:border-black focus:ring-black sm:text-sm"
-            type="text"
-            placeholder="Search..."
-          />
+          <div className="relative h-10 w-10 flex-shrink-0 cursor-pointer lg:hidden">
+            <Image
+              src="https://links.papareact.com/jjm"
+              alt="Instagram Logo"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
         </div>
-      </div>
-      {/* Right */}
-      <div>
-        <h1>Right</h1>
+        {/* Center - Search Input */}
+        <div className="max-w-xs">
+          <div className="relative mt-1 rounded-md p-3">
+            <div className="pointer-events-none absolute inset-y-0 flex items-center pl-3">
+              <SearchIcon className="h-5 text-gray-500" />
+            </div>
+            <input
+              className="block w-full rounded-md border-gray-300 bg-gray-50 pl-10 focus:border-black focus:ring-black sm:text-sm"
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
+        </div>
+        {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 cursor-pointer md:hidden" />
+          <PaperAirplaneIcon className="navBtn" />
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+        </div>
       </div>
     </div>
-    // </div>
   )
 }
