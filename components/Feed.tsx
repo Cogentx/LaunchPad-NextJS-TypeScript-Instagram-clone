c5ed18c4639af80e228bd4c3a74593c49c439c3d
Cogentx <code@cogentx.com>
2022-03-12 22:52:48 -0330

    Feed Component - mock layout

---
+++ components/Feed.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Feed() {
+  return (
+    <main>
+      {/* Section */}
+        {/* Stories */}
+        {/* Posts */}
+
+      {/* Section */}
+        {/* Mini Profile */}
+        {/* Suggestions */}
+    </main>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Feed from '../components/Feed';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
+    <div className="">
       <Head>
         <title>Instagram Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Feed />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
