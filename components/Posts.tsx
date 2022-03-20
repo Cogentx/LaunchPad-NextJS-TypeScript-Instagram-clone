ad7cc185c0801e5739fac0486d6bb784fbd02592
Cogentx <code@cogentx.com>
2022-03-20 10:26:50 -0230

    Posts Comp - Header

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import Post from './Post'
+import type { IPost } from '../custom-types';
+import Post from './Post';
 
 export default function Posts() {
+  const posts: Array<IPost> = [
+    {
+      id: '123',
+      username: 'Ted Cogent',
+      userImg: 'https://links.papareact.com/3ke',
+      img: 'https://links.papareact.com/3ke',
+      caption: 'This is Awesome!!!',
+    },
+    {
+      id: '456',
+      username: 'Ted Cogent',
+      userImg: 'https://links.papareact.com/3ke',
+      img: 'https://links.papareact.com/kxk',
+      caption: 'This is Awesome!!!',
+    },
+  ];
   return (
-    <div>
-      <Post/>
-    </div>
-  )
+    <section>
+      {posts.map((post: IPost) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
+    </section>
+  );
 }
