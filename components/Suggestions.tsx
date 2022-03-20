902730b4fe12af02c1cb166b543408d5227345ea
Cogentx <code@cogentx.com>
2022-03-20 20:33:52 -0230

    Suggestions | Suggestion Components with mock data

--- components/Suggestions.tsx
+++ components/Suggestions.tsx
@@ -1,29 +1,33 @@
 import { useEffect, useState } from 'react';
 import faker from '@faker-js/faker';
 import Suggestion from './Suggestion';
+import { ContextualCard } from '@faker-js/faker/helpers';
 
 export default function Suggestions() {
-  const [suggestions, setSuggestions] = useState<Array<any>>([]);
+  const [suggestions, setSuggestions] = useState<Array<ContextualCard>>([]);
 
   useEffect(() => {
-    const suggestions = [...Array(5)].map((_, index) => ({
-      ...faker.helpers.contextualCard(),
-      id: index,
-    }));
-    console.log(suggestions);
+    const suggestions: Array<ContextualCard> = [...Array(5)].map(
+      (_, index) => ({
+        ...faker.helpers.contextualCard(),
+        id: index,
+      })
+    );
 
     setSuggestions(suggestions);
   }, []);
 
   return (
-    <div className='mt-4 ml-10'>
-      <div className='flex justify-between text-sm mb-5'>
-        <h3 className=' text-gray-400 font-bold'>Suggestions for you</h3>
-        <button className='text-gray-600 font-semibold cursor-pointer'>See All</button>
+    <div className="mt-4 ml-10">
+      <div className="mb-5 flex justify-between text-sm">
+        <h3 className=" font-bold text-gray-400">Suggestions for you</h3>
+        <button className="cursor-pointer font-semibold text-gray-600">
+          See All
+        </button>
       </div>
 
       {suggestions.map((profile, index) => (
-        <Suggestion key={index} profile={profile}/>
+        <Suggestion key={index} username={profile.username} avatar={profile.avatar} company={profile.company}/>
       ))}
     </div>
   );
