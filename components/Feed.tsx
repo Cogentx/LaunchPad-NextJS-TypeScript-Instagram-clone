949f012b7447d3af612a335fbc69b2a99e07b17c
Cogentx <code@cogentx.com>
2022-03-24 22:04:05 -0230

    Add display logic for MiniProfile | Suggestions

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -8,7 +8,7 @@ export default function Feed() {
   const { data: session } = useSession();
 
   return (
-    <main className="mx-auto grid min-h-screen grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
+    <main className={`mx-auto grid min-h-screen grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3 ${!session && "!grid-cols-1 !max-w-3xl"}`}>
       <section className="col-span-1 md:col-span-2">
         <Stories />
         <Posts />
