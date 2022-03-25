c886301a1159ce7d7c898d91d6ec1cf7d896da70
Cogentx <code@cogentx.com>
2022-03-24 22:18:39 -0230

    403 Err | add <img referrerPolicy="no-referrer"/>

--- components/Story.tsx
+++ components/Story.tsx
@@ -6,8 +6,13 @@ interface IProps {
 export default function Story({ img, username }: IProps) {
   return (
     <div>
-        <img src={img} alt="user avatar" className="rounded-full border-2 border-red-500 w-14 p-[1.5px] object-contain cursor-pointer hover:scale-110 transition transform duration-200 ease-out"/>
-        <p className="text-xs w-14 truncate text-center">{username}</p>
+      <img
+        src={img}
+        alt="user avatar"
+        className="w-14 transform cursor-pointer rounded-full border-2 border-red-500 object-contain p-[1.5px] transition duration-200 ease-out hover:scale-110"
+        referrerPolicy="no-referrer"
+    />
+      <p className="w-14 truncate text-center text-xs">{username}</p>
     </div>
   );
 }
