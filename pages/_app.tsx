07c0086d0dd2f793717ecc6888ec32030715301e
Cogentx <code@cogentx.com>
2022-03-25 18:28:39 -0230

    Setup Recoil State for use in components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { RecoilRoot } from 'recoil';
 
 // Destructure 'pageProps' to get 'session' and remaining 'pageProps' as ...rest
 function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   );
 }
