129d71ae7eaeca992c1ce6ad38ea578418eb8121
Cogentx <code@cogentx.com>
2022-03-26 16:29:43 -0230

    Upload Photo Modal (headlessUI) styled w/ Tailwind

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,13 @@
-import { Fragment } from 'react';
+import { Fragment, useRef } from 'react';
 import { useRecoilState } from 'recoil';
 import { Dialog, Transition } from '@headlessui/react';
 import { modalState } from '../atoms/modalAtoms';
 
 export default function Modal() {
   const [open, setOpen] = useRecoilState(modalState);
+  const filePickerRef = useRef();
   const closeModal = () => setOpen(false);
+  const addImageToPost = () => console.log('adding image to post...');
 
   return (
     <Transition appear show={open} as={Fragment}>
@@ -44,24 +46,32 @@ export default function Modal() {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block w-full max-w-md transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full">
-              <Dialog.Title
-                as="h3"
-                className="text-lg font-medium leading-6 text-gray-900"
-              >
-                Title
-              </Dialog.Title>
-
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">Message...</p>
-              </div>
-              <div className="mt-4">
-                <button
-                  type="button"
-                  className="inline-flex  justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                >
-                  Got itasdfdsfasdfasdfd!!!
-                </button>
+            <div className="inline-block w-full max-w-md transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:align-middle">
+              <div>
+                <div className="mt-3 text-center sm:mt-5">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Upload a photo
+                  </Dialog.Title>
+                </div>
+                <div className="mt-2">
+                  <input
+                    ref={filePickerRef}
+                    type="file"
+                    hidden
+                    onChange={addImageToPost}
+                  />
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-300 hover:disabled:bg-gray-300 sm:text-sm"
+                  >
+                    Upload Post
+                  </button>
+                </div>
               </div>
             </div>
           </Transition.Child>
