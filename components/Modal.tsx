e22918ae2354d4c2fa55f601430e90ace30e94e2
Cogentx <code@cogentx.com>
2022-03-26 21:17:29 -0230

    Add File Picker functionality

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,14 +1,22 @@
-import { Fragment, useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { Dialog, Transition } from '@headlessui/react';
 import { modalState } from '../atoms/modalAtoms';
+import { CameraIcon } from '@heroicons/react/outline';
 
 export default function Modal() {
   const [open, setOpen] = useRecoilState(modalState);
+  const [selectedFile, setSelectedFile] = useState(null);
   const filePickerRef = useRef(null);
   const captionRef = useRef(null);
   const closeModal = () => setOpen(false);
-  const addImageToPost = () => console.log('adding image to post...');
+  const addImageToPost = (e: any) => console.log('adding image to post...', e);
+  const handleCameraClick = () => {
+    if (filePickerRef.current) {
+      const filePicker = filePickerRef.current as HTMLInputElement;
+      filePicker.click();
+    }
+  };
 
   return (
     <Transition appear show={open} as={Fragment}>
@@ -49,37 +57,48 @@ export default function Modal() {
           >
             <div className="inline-block w-full max-w-md transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:align-middle">
               <div>
-                <div className="mt-3 text-center sm:mt-5">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Upload a photo
-                  </Dialog.Title>
-                </div>
-                <div className="mt-2">
-                  <input
-                    ref={filePickerRef}
-                    type="file"
-                    hidden
-                    onChange={addImageToPost}
+                <div
+                  onClick={handleCameraClick}
+                  className="mx-auto flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-red-100"
+                >
+                  <CameraIcon
+                    aria-hidden="true"
+                    className="h-6 w-6 text-red-600"
                   />
                 </div>
-                <div className="mt-2">
-                  <input
-                    ref={captionRef}
-                    type="text"
-                    placeholder="Please enter a caption..."
-                    className="w-full border-none text-center focus:ring-0"
-                  />
-                </div>
-                <div className="mt-5 sm:mt-6">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-300 hover:disabled:bg-gray-300 sm:text-sm"
-                  >
-                    Upload Post
-                  </button>
+                <div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Upload a photo
+                    </Dialog.Title>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      ref={filePickerRef}
+                      type="file"
+                      hidden
+                      onChange={addImageToPost}
+                    />
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      ref={captionRef}
+                      type="text"
+                      placeholder="Please enter a caption..."
+                      className="w-full border-none text-center focus:ring-0"
+                    />
+                  </div>
+                  <div className="mt-5 sm:mt-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-300 hover:disabled:bg-gray-300 sm:text-sm"
+                    >
+                      Upload Post
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
